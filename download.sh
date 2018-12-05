7a55ea0414ee634f7b08e8bc8b4210a193d37276
Kosuke Kushizaki <13391342+wisetlaloc@users.noreply.github.com>
2018-12-06 00:05:49 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -38,5 +38,5 @@ fi
 # mac-auto-setup.git
 #
 echo " ---- mac-auto-setup.git -----"
-git clone https://github.com/AkkeyLab/mac-auto-setup.git
+git clone https://github.com/wisetlaloc/mac-auto-setup.git
 echo " ------------ END ------------"
