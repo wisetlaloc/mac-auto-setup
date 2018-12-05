23e15fce64e99795693cd2ecd0ae9478cb839a2a
Kosuke Kushizaki <13391342+wisetlaloc@users.noreply.github.com>
2018-12-06 01:00:49 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -15,6 +15,12 @@ git checkout master
 git pull origin master
 
 #
+# private.zsh update
+# TODO!! make private.bash
+# cp $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/settings/zsh/private.zsh ~/.yadr/zsh/private.zsh
+# source ~/.zshrc
+
+#
 # Homebrew update and upgrade
 #
 brew upgrade --cleanup
