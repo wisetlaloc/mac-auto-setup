ce53a3344bc67225053bb56207111f50cbcb275c
Kosuke Kushizaki <13391342+wisetlaloc@users.noreply.github.com>
2018-12-06 00:34:41 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -15,15 +15,9 @@ git checkout master
 git pull origin master
 
 #
-# private.zsh update
-#
-cp $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/settings/zsh/private.zsh ~/.yadr/zsh/private.zsh
-source ~/.zshrc
-
-#
 # Homebrew update and upgrade
 #
-brew upgrade --creanup
+brew upgrade --cleanup
 
 #
 # App Store app upgrade
